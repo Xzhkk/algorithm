e218e754a3042501600e790b9c8b5ac4fe9e6cbe
egg <846916498@qq.com>
2020-10-18 17:34:17 +0800

    快速排序

---
+++ src/Code12_fastSort.java
@@ -0,0 +1,53 @@
+public class Code12_fastSort {
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static int[] netherlandsFlag(int[] arr, int L, int R) {
+        if (L > R) {
+            return new int[] { -1, -1 };
+        }
+        if (L == R) {
+            return new int[] { L, R };
+        }
+        int less = L - 1; // < 区 右边界
+        int more = R;     // > 区 左边界
+        int index = L;
+        while (index < more) {
+            if (arr[index] == arr[R]) {
+                index++;
+            } else if (arr[index] < arr[R]) {
+                swap(arr, index++, ++less);
+            } else { // >
+                swap(arr, index, --more);
+            }
+        }
+        swap(arr, more, R);
+        return new int[] { less + 1,more};
+    }
+
+    public static void process3(int[] arr, int L , int R) {
+        if (L >= R) {
+            return;
+        }
+        swap(arr, L + (int) (Math.random() * (R - L + 1)), R);
+        int[] equalArea = netherlandsFlag(arr, L, R);
+        process3(arr, L, equalArea[0] - 1);
+        process3(arr, equalArea[1] + 1, R);
+    }
+
+    public static void quickSort(int[] arr) {
+        process3(arr,0,arr.length-1);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4,9,7,3,2};
+        quickSort(arr);
+        for (int i : arr) {
+            System.out.print(i+"\t");
+        }
+    }
+}
